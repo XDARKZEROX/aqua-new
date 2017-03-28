eb29abde027f094b0a4f1077b5a0e0f690c9fa60
Alexander Guzman <a.guzman@costamar.com>
2017-03-28 11:49:03 -0500

    Ahora se llama a la plantilla Index y ya no directamente a la plantilla Master

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,1 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>AQUA FIT PH9</title>
-</head>
-<body>
-
-    <h1>This is a test</h1>
-
-</body>
-</html>
-
+@extends('layouts.master')
